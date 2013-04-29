d1f2b35ffec99b40d7857b47282f5d907acf4520
Carl Lumma <clumma@gmail.com>
2013-04-29 15:58:29 -0700

    server complete

--- tipsbeta.js
+++ tipsbeta.js
@@ -1,53 +1,79 @@
-var getSeries = function(seriesName) {
-  var seriesUrl = 'http://api.stlouisfed.org/fred/series/observations?series_id=';
-  var seriesCode = 'DGS5';
-  if (seriesName === 'tips') {seriesCode = 'DFII5'};
-  if (seriesName === 'sp500') {seriesCode = 'SP500'};
-  var fromDate = '&observation_start=2003-01-01';
-  var apiKey = '&api_key=c2b3e4b899953f52dc9740abab6c28eb';
-  var url = seriesUrl + seriesCode + fromDate + apiKey;
-  Meteor.call('checkFred', url, function(error, result) {
-    error && console.log(error);
-    Session.set(seriesName, result);
-  });
+if (Meteor.isClient) {
+
+	// a seriesInfo object
+	var betaSeries = {
+		name: 'betaSeries',
+		fromDate: '2003-01-01',
+		series: {
+			notes: 'DGS5',
+			tips: 'DFII5',
+			sp500: 'SP500'
+		}
+	};
+
+	var getData = function(seriesInfo) {
+		Meteor.call('checkFred', seriesInfo, function(error, result) {
+			error && console.log(error);
+			Session.set(seriesInfo.name, result);
+		});
+	};
+
+	getData(betaSeries);
+
+	Template.home.chart = function() {
+		var data = Session.get('betaSeries');
+		if (!data) return 'loading...';
+		console.log(data.notes[1]);
+		console.log(data.tips.length);
+		console.log(data.sp500.length);
+		return data.notes;
+	};
+	Template.home.events({
+ 		'click input' : function() {
+			// template data, if any, is available in 'this'
+			console.log('you clicked');
+		}
+	});
 }
 
-if (Meteor.isClient) {
-  getSeries('notes');
-  getSeries('tips');
-  getSeries('sp500');
-  Template.home.chart = function() {
-    var data = Session.get('sp500');
-    if (!data) return 'loading...';
-    return data[0].date;
-  };
-  Template.home.events({
-    'click input' : function() {
-      // template data, if any, is available in 'this'
-      console.log('you clicked');
-    }
-  });
-}
+/* a fredCache object
+{
+	seriesInfoName: {
+		date: number,
+		results: {
+			notes: obj,
+			tips: obj,
+			sp500: obj
+		}
+	},
+	...
+} */
 
 if (Meteor.isServer) {
-  Meteor.startup(function() {
-    // code to run on server at startup
-    var lastChecked = 0;
-    var resultCache = {};
-    Meteor.methods({checkFred: function(url) {
-      this.unblock();
-      var result = {};
-      var fresh = (Date.now() - lastChecked < 3600000);
-      if (resultCache.hasOwnProperty(url) && fresh) {
-        result = resultCache[url];
-      } else {
-        result = Meteor.http.call('GET', url);
-        result = XML2JS.parse(result.content);
-        result = _.pluck(result.observations.observation, '$');
-        resultCache[url] = result;
-        lastChecked = Date.now();
-      }
-      return result;
-    }});
-  });
+	Meteor.startup(function() {
+		// code to run on server at startup
+		var fredCache = {};
+		Meteor.methods({checkFred: function(seriesInfo) {
+			this.unblock();
+			if (fredCache.hasOwnProperty(seriesInfo.name)) {
+				if (Date.now() - fredCache[seriesInfo.name].date < 3600000) {
+					return fredCache[seriesInfo.name].results;
+				}
+			}
+			fredCache[seriesInfo.name] = {};
+			fredCache[seriesInfo.name].results = {};
+			for (key in seriesInfo.series) {
+				var url = 'http://api.stlouisfed.org/fred/series/observations?series_id=';
+				fromDate = '&observation_start=' + seriesInfo.fromDate;
+				var apiKey = '&api_key=c2b3e4b899953f52dc9740abab6c28eb';
+				url = url + seriesInfo.series[key] + fromDate + apiKey;
+				var result = Meteor.http.call('GET', url);
+				result = XML2JS.parse(result.content);
+				result = _.pluck(result.observations.observation, '$');
+				fredCache[seriesInfo.name].results[key] = result;
+			}
+			fredCache[seriesInfo.name].date = Date.now();
+			return fredCache[seriesInfo.name].results;
+		}});
+	});
 }
