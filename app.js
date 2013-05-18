a457a4465b3bc901b1ccc8af46cadcd5b177e3d2
Carl Lumma <clumma@gmail.com>
2013-05-18 16:41:55 -0700

    crappy commit from shawn

--- app.js
+++ app.js
@@ -33,10 +33,32 @@ if (Meteor.isClient) {
 		var data = Session.get('betaSeries');
 		if (!data) return 'fetching current data...';
 		console.log(data.notes[1]);
-		console.log(data.tips.length);
-		console.log(data.sp500.length);
+		// console.log(data.tips.length);
+		// console.log(data.sp500.length);
 		return data.notes;
 	};
+
+	var drawChart = function() {
+		var data = Session.get('betaSeries');
+		console.log(data);
+		$('.container').highcharts('StockChart', {
+				rangeSelector : {
+					selected : 1
+				},
+				title : {
+					text : 'AAPL Stock Price'
+				},
+				series : [{
+					name : 'Notes',
+					data : data.notes,
+					tooltip: {
+						valueDecimals: 1
+					}
+				}]
+		});
+	};
+
+	drawChart();
 
 }
 
@@ -68,10 +90,11 @@ if (Meteor.isServer) {
 			fredCache[seriesInfo.name] = {};
 			fredCache[seriesInfo.name].results = {};
 			for (key in seriesInfo.series) {
-				var url = 'http://api.stlouisfed.org/fred/series/observations?series_id=';
-				fromDate = '&observation_start=' + seriesInfo.fromDate;
+				var url = 'http://api.stlouisfed.org/fred/series/observations';
+				var seriesUri = '?series_id=' + seriesInfo.series[key];
+				var dateUri = '&observation_start=' + seriesInfo.fromDate;
 				var apiKey = '&api_key=c2b3e4b899953f52dc9740abab6c28eb';
-				url = url + seriesInfo.series[key] + fromDate + apiKey;
+				url = url + seriesUri + dateUri + apiKey;
 				var result = Meteor.http.call('GET', url);
 				result = XML2JS.parse(result.content);
 				result = _.pluck(result.observations.observation, '$');
